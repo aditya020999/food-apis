7fadfb7ad75e3ff195d59979f29252ba522379ea
Aditya <adityaprajpoot@gmail.com>
2024-10-13 10:28:21 +0530

    first master commit

---
+++ src/main/java/com/food/food_apis/FoodApisApplication.java
@@ -0,0 +1,13 @@
+package com.food.food_apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodApisApplication.class, args);
+	}
+
+}
